4fa2f701717b098a09c0872d7ba88a879a8ee365
Kancha98 <emkanchana98@gmail.com>
2025-05-06 22:41:53 +0530

    Update app_s.py

--- app_s.py
+++ app_s.py
@@ -484,9 +484,9 @@ try:
     # Rename headers
     filtered_df.columns = [col.replace('_', ' ').title() for col in filtered_df.columns]
 
-    #numeric_columns = [
-    #'Closing Price', 'Prev Close', 'Turnover'
-    #]
+    numeric_columns = [
+    'Closing Price', 'Prev Close', 'Turnover'
+    ]
     
     for col in numeric_columns:
         if col in filtered_df.columns:
@@ -500,10 +500,7 @@ try:
     for col in filtered_df.select_dtypes(include=['float64', 'int64']).columns:
         filtered_df[col] = filtered_df[col].apply(lambda x: f"{x:,.2f}" if isinstance(x, float) else f"{x:,}")
 
-    filtered_df = filtered_df.drop(columns=[col for col in ['Vol Avg 5D','Vol Avg 20D', 'Ema 20', 'Ema 50', 
-                                                            'Ema 100', 'Ema 200', 'Last Updated','Date', 'Symbol', 
-                                                            'Closing Price', 'Prev Close', 'Change Pct', 'Turnover', 
-                                                            'Volume', 'Volume Analysis', 'Rsi', 'Rsi Divergence', 'Relative Strength'] if col in filtered_df.columns])    
+    
     # Display the filtered table
     st.dataframe(filtered_df, use_container_width=True)
     
